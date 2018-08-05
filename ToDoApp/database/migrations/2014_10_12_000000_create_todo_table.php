8082a1c497007412a6b880eabc8de8c79ac5340b
RubensVdM <rubenvdm@telenet.be>
2018-08-05 17:37:57 +0200

    ToDo Database

---
+++ ToDoApp/database/migrations/2014_10_12_000000_create_todo_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToDoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo', function (Blueprint $table) {
+            $table->string('title');
+            $table->text('description');
+            $table->increments('id');
+            $table->timestamps();
+            $table->SoftDeletes();
+            /*
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo');
+    }
+}
